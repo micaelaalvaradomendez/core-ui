0b0224c423b2312cf45512217a54d6f26f886059
Micaela <micaelaalvaradomendez@gmail.com>
2025-07-04 10:22:01 -0300

    version 0 del core-UI

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// definicion de que es lo que vamos a exportar
+// los css no se exportan, cuando se intala la dependcia se hace un import
+//cuando se intale la dependencia estos son los elementos que va a poder usar el proyecto
+
+export {default as navbar} from './src/components/navbar.svelte';
+export {default as footer} from './src/components/footer.svelte';
